43901df25ed665e8c305b3e3d54af43374be3b06
KShreyas07 <khandekarshreyas05@gmail.com>
2026-04-15 22:58:13 +0530

    Update TestGoogle.java

--- TestGoogle.java
+++ TestGoogle.java
@@ -1,19 +1,23 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestGoogle {
+
     public static void main(String[] args) {
-        // Set ChromeDriver path
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
+
+        WebDriverManager.chromedriver().setup();
+
         WebDriver driver = new ChromeDriver();
-        // Open Google
+
         driver.get("https://www.google.com");
-        // Search for Jenkins
+
         driver.findElement(By.name("q")).sendKeys("Jenkins");
         driver.findElement(By.name("q")).submit();
-        // Print title
-        System.out.println("Page Title: " + driver.getTitle());
-        // Close browser
+
+        System.out.println("Title: " + driver.getTitle());
+
         driver.quit();
     }
 }
