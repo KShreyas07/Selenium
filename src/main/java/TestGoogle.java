38349e531d5917130d6e6459f5952e8573d9c204
KShreyas07 <khandekarshreyas05@gmail.com>
2026-04-16 11:09:11 +0530

    Update TestGoogle.java

--- src/main/java/TestGoogle.java
+++ src/main/java/TestGoogle.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TestGoogle {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\path\\chromedriver.exe");
-
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.google.com");
