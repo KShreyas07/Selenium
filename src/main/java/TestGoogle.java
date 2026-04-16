a59a4990cb51219d118269b502e89fdb46fb6512
KShreyas07 <khandekarshreyas05@gmail.com>
2026-04-16 11:07:40 +0530

    Update TestGoogle.java

--- src/main/java/TestGoogle.java
+++ src/main/java/TestGoogle.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestExample {
+public class TestGoogle {
     public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver", "C:\\path\\chromedriver.exe");
